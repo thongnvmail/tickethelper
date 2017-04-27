ae2bd2d76b9fd80b98d4efdc75238f98dfe4552f
ngo van bon <ngovanbon16@gmail.com>
2017-04-27 13:17:41 +0700

    them giao dien quan ly (giao dien tinh chua xu li xu kien)

--- app.js
+++ app.js
@@ -6,13 +6,17 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
+var bookcar = require('./routes/bookcar');
 var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
+
+// use public folder
+// app.use(express.static('views/assets'));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -23,6 +27,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+app.use('/bookcar', bookcar);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
@@ -44,3 +49,5 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+app.listen(process.env.PORT || 3000);
